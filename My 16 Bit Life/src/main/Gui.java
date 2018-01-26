87c7c6bdf860814ff12bb09b457586115275c132
nathan <ootisg@illcomics.com>
2018-01-25 19:21:14 -0600

    Added a new weapon

--- My 16 Bit Life/src/main/Gui.java
+++ My 16 Bit Life/src/main/Gui.java
@@ -11,7 +11,7 @@ public class Gui extends GameObject {
 	ListTbox mainMenu;
 	ListTbox subMenu;
 	Tbox splashBox;
-	String[] weaponArray = new String[] {"REDBLACK PAINTBALL GUN", "STABBY KNIFE", "BACK"};
+	String[] weaponArray = new String[] {"REDBLACK PAINTBALL GUN", "STABBY KNIFE", "A NOTE FROM SAM", "BACK"};
 	String[] itemArray = new String[] {"PEACH JUICE", "HEALTH JUICE", "BACK"};
 	public int focus = 0;
 	public Gui () {
@@ -35,22 +35,27 @@ public class Gui extends GameObject {
 				subMenu = new ListTbox (16, 16, weaponArray);
 			}
 			int subVal = subMenu.getSelected ();
-			if (subVal == 0 && focus == 1) {
+			if (focus == 1 && subMenu.getSelectedValue ().equals ("REDBLACK PAINTBALL GUN")) {
 				focus = 2;
 				splashBox = new Tbox (32, 32, 18, 2, "THAT WEAPON IS    ALREADY EQUIPPED.");
 				splashBox.setScrollRate (0);
 			}
-			if (subVal == 1 && focus == 1 && weaponArray [1].equals ("STABBY KNIFE")) {
+			if (focus == 1 && subMenu.getSelectedValue ().equals ("STABBY KNIFE")) {
 				focus = 2;
 				splashBox = new Tbox (32, 32, 16, 2, "THE STABBY KNIFERAN AWAY.");
 				splashBox.setScrollRate (0);
 			}
-			if (subVal == 1 && focus == 2 && weaponArray [1].equals ("STABBY KNIFE") && !splashBox.declared) {
-				weaponArray = new String[] {"REDBLACK PAINTBALL GUN", "BACK"};
+			if (focus == 1 && subMenu.getSelectedValue ().equals ("A NOTE FROM SAM")) {
+				focus = 2;
+				splashBox = new Tbox (32, 32, 64, 16, "I GET THAT WE HAVE ONLY 3 CHARACTERS, BUT WE CURRENTLY HAVE 25  WEAPONS, AND FROM WHAT I CAN TELL, 21 OF THEM ARE FOR JEFFREY   AND 4 ARE FOR ME. AND NONE FOR RYAN SO FAR. I KNOW THAT THERE   ARE WEAPONS THAT EVERY CHARACTER CAN USE, BUT I THINK WE SHOULD HAVE, MAYBE, 7 OR 8 CHARACTER-SPECIFIC WEAPONS? I MEAN, I THINK IT COULD BE SOMETHING LIKE A TOTAL OF 30 OR 35 WEAPONS. 8 FOR   ME, 8 FOR JEFFREY AND 8 FOR RYAN WILL MAKE 24 WEAPONS ALREADY.  THIS LEAVES 6-11 OPEN SLOTS FOR WEAPONS WE COULD ALL USE. NOW   I'M NOT SAYING EVERY SINGLE ONE OF THOSE WEAPONS JEFFREY HAS    WILL BE USED OR EVEN JUST FOR JEFFREY, AND I DO KNOW THAT THEY  WERE MADE OVER THE SUMMER RAPIDLY, BUT I FEEL LIKE WE SHOULD    ALSO TRY ON WORKING ON ONE WEAPON AT A TIME. TRY TO MAKE IT     BALANCED AND GIVE IT THE 7 DIFFERENT UPGRADE LINES BEFORE MOVING TO THE NEXT ONE. THAT'S WHAT I HAVE DONE FOR MY WEAPONS, AND   AFTER ALL THIS TIME, I HAVE MANAGED TO CREATE 4 WEAPONS THAT I  FEEL LIKE ARE NICELY BALANCED. HOWEVER,");
+				splashBox.setScrollRate (0);
+			}
+			if (focus == 2 && subMenu.getSelectedValue ().equals ("STABBY KNIFE") && !splashBox.declared) {
+				weaponArray = removeElement (weaponArray, subMenu.getSelected ());
 				subMenu.forget ();
 				subMenu = new ListTbox (16, 16, weaponArray);
 			}
-			if (subMenu.getSelected () == 2 && weaponArray [2].equals ("BACK") && !splashBox.declared) {
+			if (subMenu.getSelectedValue ().equals ("BACK") && !splashBox.declared) {
 				subMenu.forget ();
 				focus = 0;
 				mainMenu.deselect ();
